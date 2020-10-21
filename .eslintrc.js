398d7fba5b8dd2e4d9ba7317b9051ea6fad26fdf
Hamish Macpherson <hamstu@gmail.com>
2020-10-21 16:03:48 -0700

    ignore lib in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,15 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", "airbnb", "prettier", "prettier/react"],
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'prettier/react'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react"],
+  plugins: ['react'],
   rules: {},
+  ignorePatterns: ['lib/*'], // Stop ESLint complaining when looking at transpiled lib
 };
