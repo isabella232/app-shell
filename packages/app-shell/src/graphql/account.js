d3d3482c187946b71f9650657e5cfdb2accc857d
Ana Almeida <anafilipadealmeida@gmail.com>
2021-02-23 09:40:50 +0000

    Modify org switcher mutation

--- packages/app-shell/src/graphql/account.js
+++ packages/app-shell/src/graphql/account.js
@@ -1,10 +1,10 @@
-import gql from 'graphql-tag'
+import gql from 'graphql-tag';
 
 export const SET_CURRENT_ORGANIZATION = gql`
   mutation AccountSetCurrentOrganization($organizationId: String) {
     accountSetCurrentOrganization(organizationId: $organizationId)
   }
-`
+`;
 
 export const QUERY_ACCOUNT = gql`
   query GetAccount {
@@ -23,6 +23,9 @@ export const QUERY_ACCOUNT = gql`
       organizations {
         id
         name
+        canEdit
+        role
+        createdAt
       }
       products {
         name
--- packages/app-shell/src/useOrgSwitcher.js
+++ packages/app-shell/src/useOrgSwitcher.js
@@ -1,27 +1,36 @@
-import { useMutation, useApolloClient } from '@apollo/client'
+import { useMutation, useApolloClient } from '@apollo/client';
 import { QUERY_ACCOUNT, SET_CURRENT_ORGANIZATION } from './graphql/account';
 
 function useOrgSwitcher() {
-  const apolloClient = useApolloClient()
-  const [setCurrentOrganization] = useMutation(
-    SET_CURRENT_ORGANIZATION, {
-      client: apolloClient,
-      refetchQueries: [{
-        query: QUERY_ACCOUNT,
-      }],
-    }
-  )
+  const apolloClient = useApolloClient();
+  const [setCurrentOrganization] = useMutation(SET_CURRENT_ORGANIZATION);
+
+  const updateCache = (organizationId) => {
+    const previousData = apolloClient.readQuery({ query: QUERY_ACCOUNT });
+    const organizationSelected = previousData?.account?.organizations?.filter(
+      (organization) => organization.id === organizationId
+    )[0];
+    const updatedData = {
+      account: {
+        ...previousData.account,
+        currentOrganization: organizationSelected,
+      },
+    };
+    apolloClient.writeQuery({
+      query: QUERY_ACCOUNT,
+      data: updatedData,
+    });
+  };
 
   return async (organizationId, options = {}) => {
     await setCurrentOrganization({
       variables: {
         organizationId,
-      }
-    })
-    if (options.onCompleted) {
-      options.onCompleted(organizationId)
-    }
-  }
+      },
+      update: () => updateCache(organizationId),
+      onCompleted: () => options.onCompleted(organizationId),
+    });
+  };
 }
 
-export default useOrgSwitcher
+export default useOrgSwitcher;
