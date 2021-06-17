c1ca32e47651dd6af287d71406b4a9a6c3fb90bb
Federico Weber <federicoweber@gmail.com>
2021-06-17 14:14:05 -0700

    upload assets to s3

--- packages/app-shell/webpack.config.prod.js
+++ packages/app-shell/webpack.config.prod.js
@@ -1,11 +1,19 @@
 const webpack = require('webpack');
+const path = require('path');
 const { merge } = require('webpack-merge');
-const common = require('./webpack.config.common.js');
+const common = require('./webpack.config.common');
 
-const mode = 'development';
+const PATH_BUILD = path.resolve(__dirname, "build")
+
+const mode = 'production';
 
 const merged = merge(common, {
   mode,
+  output: {
+    filename: './[name].[fullhash].js',
+    clean: true,
+    path: PATH_BUILD,
+  },
   plugins: [
     new webpack.DefinePlugin({
       GRAPHQL_API: JSON.stringify('https://graph.buffer.com'),
