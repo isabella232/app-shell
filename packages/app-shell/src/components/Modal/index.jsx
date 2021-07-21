0d5b03c033422f3858ae6e44721e39cfcd76a8f9
Federico Weber <federicoweber@gmail.com>
2021-07-21 09:27:58 -0700

    fix trial expired rendering

--- packages/app-shell/src/components/Modal/index.jsx
+++ packages/app-shell/src/components/Modal/index.jsx
@@ -34,11 +34,11 @@ const Modal = ({ modal, openModal }) => {
 
   useEffect(() => {
     const hasDismissedTrialModal = getCookie({ key: 'trialOverDismissed' })
-    const isAwaitingUserAction = true;
+    const isAwaitingUserAction = user?.currentOrganization?.billing?.subscription?.trial?.isAwaitingUserAction || false;
     if (!hasDismissedTrialModal && isAwaitingUserAction) {
       openModal(MODALS.trialExpired)
     }
-  }, [user.loading]);
+  }, [user.load]);
 
   useEffect(() => {
     setHasModal(!!modal);
