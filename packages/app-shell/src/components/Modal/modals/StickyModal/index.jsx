542c6fcbc00ce9b69633f252f9e50db49b6deb47
Gisete Kindahl <gisete@buffer.com>
2021-08-17 12:41:48 -0700

    Deleted style file to clear eslint error

--- packages/app-shell/src/components/Modal/modals/StickyModal/index.jsx
+++ packages/app-shell/src/components/Modal/modals/StickyModal/index.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+import { gray } from '@bufferapp/ui/style/colors';
 
-import { Container } from './style';
+const Container = styled.div`
+  position: fixed;
+  bottom: 37px;
+  right: 37px;
+  border: 1px solid ${gray};
+  border-radius: 4px;
+  z-index: 9;
+  box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.16);
+  
+  > div:first-of-type {
+    border-radius: 4px;
+  }
+`;
 
 const StickyModal = ({ children }) => {
   return <Container>{children}</Container>;
