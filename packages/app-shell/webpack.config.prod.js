46f6aede1f42d1458e627074c68433c93fcc74bb
Juliana Gomez <juliana@gomezjuliana.com>
2021-09-27 11:10:54 -0500

    dummy commit

--- packages/app-shell/webpack.config.prod.js
+++ packages/app-shell/webpack.config.prod.js
@@ -4,7 +4,6 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.config.common');
 
 const PATH_BUILD = path.resolve(__dirname, 'build');
-
 const mode = 'production';
 
 const merged = merge(common, {
