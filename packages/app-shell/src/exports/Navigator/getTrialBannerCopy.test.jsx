ffcd97e13ac4b30dd545f80d22aa9103bb492ee0
Henry Chea <cheahenry@gmail.com>
2021-12-09 12:09:50 +0100

    extract trail banner copy to a function

---
+++ packages/app-shell/src/exports/Navigator/getTrialBannerCopy.test.jsx
@@ -0,0 +1,11 @@
+import { renderHook } from '@testing-library/react-hooks';
+import getTrialBannerCopy from './getTrialBannerCopy';
+
+describe('getTrialBannerCopy', () => {
+  it('should the correct trail banner copy', () => {
+    expect(getTrialBannerCopy({ planName: 'Essentials + Team Pack', daysRemaining: 1 })).toBe('You are on the Essentials plan with Team pack trial with 1 day left. Add your billing details now to start your subscription.');
+    expect(getTrialBannerCopy({ planName: 'Essentials + Team Pack', daysRemaining: 13 })).toBe('You are on the Essentials plan with Team pack trial with 13 days left. Add your billing details now to start your subscription.');
+    expect(getTrialBannerCopy({ planName: 'Essentials', daysRemaining: 10 })).toBe('You are on the Essentials plan trial with 10 days left. Add your billing details now to start your subscription.');
+    expect(getTrialBannerCopy({ planName: 'Agency', daysRemaining: 10 })).toBe('You are on the Agency plan trial with 10 days left. Add your billing details now to start your subscription.');
+    });
+});
