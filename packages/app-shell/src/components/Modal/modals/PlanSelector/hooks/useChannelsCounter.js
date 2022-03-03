ca0f91f618b9214c6b11231c089724af2e50c558
Daisy Smith <daisymarie128@gmail.com>
2022-03-03 15:42:35 +1000

    [UPDATE] Add main logic to handle channel messaging

--- packages/app-shell/src/components/Modal/modals/PlanSelector/hooks/useChannelsCounter.js
+++ packages/app-shell/src/components/Modal/modals/PlanSelector/hooks/useChannelsCounter.js
@@ -1,7 +1,22 @@
 import { useState } from 'react';
 
-const useChannelsCounter = (initialChannelCount = 0, minimumQuantity = 1) => {
+import {
+  getFreePlanChannelInputMessaging,
+  getAgencyPlanMinimumChannelInputMessaging,
+} from '../../../utils';
+
+const agencyPlanLimitMessageStatus =
+  getAgencyPlanMinimumChannelInputMessaging();
+const freePlanLimitMessageStatus = getFreePlanChannelInputMessaging();
+
+const useChannelsCounter = (
+  planId,
+  initialChannelCount = 0,
+  minimumQuantity = 1
+) => {
   const [channelsCount, setChannelsCount] = useState(initialChannelCount);
+  const [channelCountMessageStatus, setChannelCountMessageStatus] =
+    useState(null);
 
   function setChannelsCounterValue(value) {
     setChannelsCount(value);
@@ -10,12 +25,44 @@ const useChannelsCounter = (initialChannelCount = 0, minimumQuantity = 1) => {
   function increaseCounter(disableIncrease = false) {
     if (!disableIncrease) {
       setChannelsCount(channelsCount + 1);
+
+      if (
+        planId === 'free' &&
+        channelsCount >= 3 &&
+        channelCountMessageStatus === null
+      ) {
+        setChannelCountMessageStatus(freePlanLimitMessageStatus);
+      }
+
+      if (
+        planId === 'agency' &&
+        channelsCount >= 10 &&
+        channelCountMessageStatus !== null
+      ) {
+        setChannelCountMessageStatus(null);
+      }
     }
   }
 
   function decreaseCounter(disableDecrease = false) {
     if (channelsCount > minimumQuantity || disableDecrease) {
       setChannelsCount(channelsCount - 1);
+    }
+
+    if (
+      planId === 'free' &&
+      channelsCount <= 4 &&
+      channelCountMessageStatus !== null
+    ) {
+      setChannelCountMessageStatus(null);
+    }
+
+    if (
+      planId === 'agency' &&
+      channelsCount === 10 &&
+      channelCountMessageStatus === null
+    ) {
+      setChannelCountMessageStatus(agencyPlanLimitMessageStatus);
     }
   }
 
@@ -24,6 +71,8 @@ const useChannelsCounter = (initialChannelCount = 0, minimumQuantity = 1) => {
     setChannelsCounterValue,
     increaseCounter,
     decreaseCounter,
+    channelCountMessageStatus,
+    setChannelCountMessageStatus,
   };
 };
 
