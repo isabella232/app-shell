2b51baa4321075ddf003c7d81608fd5dc564e3a3
Gisete Kindahl <gisete@buffer.com>
2022-03-07 11:24:07 -0300

    Moved main code into its own component, CSS updates

---
+++ packages/app-shell/src/components/Modal/modals/QuantityUpdate/components/style.js
@@ -0,0 +1,79 @@
+import styled from 'styled-components';
+import { fontSizeSmall } from '@bufferapp/ui/style/fonts';
+import {
+  grayLighter,
+  grayLight,
+  white,
+  blue,
+} from '@bufferapp/ui/style/colors';
+
+export const Header = styled.div`
+  padding: 16px;
+  max-width: 422px;
+
+  a {
+    font-family: Roboto, sans-serif;
+    font-size: 14px;
+    font-weight: 700;
+    line-height: 16px;
+    cursor: pointer;
+    text-decoration: none;
+    color: ${blue};
+  }
+`;
+
+export const Title = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  margin-bottom: 20px;
+
+  span {
+    margin-right: 15px;
+  }
+`;
+
+export const Icons = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  svg {
+    width: 12px;
+    height: 12px;
+    margin-right: 6px;
+
+    &:last-of-type {
+      margin-right: 0;
+    }
+  }
+`;
+
+export const SectionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: ${grayLighter};
+  width: 100%;
+  font-size: ${fontSizeSmall};
+`;
+
+export const Section = styled.div`
+  background-color: ${white};
+  margin: 16px;
+  border: 1px ${grayLight} solid;
+  border-radius: 4px;
+`;
+
+export const InnerContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 16px;
+  font-size: ${fontSizeSmall};
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  padding: 16px 0;
+  justify-content: end;
+  padding-right: 20px;
+`;
