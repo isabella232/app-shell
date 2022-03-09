90913dee22c443d04a31a62a1c81b0c6bf8288db
Daisy Smith <daisymarie128@gmail.com>
2022-03-09 12:29:36 +1100

    Fix/z index modals (#185)
    * [UPDATE] Increased modal zindex
    This was falling under the nav bar when users zoomed in
    
    * [UPDATE] Remove zindex and make modal scrollable

--- packages/app-shell/src/components/Modal/index.jsx
+++ packages/app-shell/src/components/Modal/index.jsx
@@ -22,9 +22,15 @@ import TrialExpired from './modals/TrialExpired';
 import QuantityUpdate from './modals/QuantityUpdate';
 import { Paywall } from './modals/Paywall';
 
-import { shouldShowFreeUserStartTrialPrompt, shouldShowChannelConnectionPrompt, shouldShowPaywallModal } from './utils';
+import {
+  shouldShowFreeUserStartTrialPrompt,
+  shouldShowChannelConnectionPrompt,
+  shouldShowPaywallModal,
+} from './utils';
 
 const ModalWrapper = styled.div`
+  min-height: 500px;
+
   > div {
     z-index: 1;
   }
@@ -183,7 +189,9 @@ const Modal = React.memo(({ modal, openModal }) => {
   return (
     <>
       {hasModal && (
-        <ModalWrapper><ModalContent modal={modal} closeAction={() => openModal(null)} /></ModalWrapper>
+        <ModalWrapper>
+          <ModalContent modal={modal} closeAction={() => openModal(null)} />
+        </ModalWrapper>
       )}
     </>
   );
